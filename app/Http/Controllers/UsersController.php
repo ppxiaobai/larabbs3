276adfa6ac659abc27226d6b5244fa6cf77a2b8e
ppxiaobai <309997474@qq.com>
2021-04-01 11:45:27 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,14 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     // 个人页面
-    public function show(User $user){
+    public function show(User $user)
+    {
 
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
+    }
+
+    // 编辑页面
+    public function edit(User $user)
+    {
+
+        return view('users.edit', compact('user'));
+    }
+
+    // 编辑保存
+    public function update(UserRequest $request,User $user){
+
+        $user->update($request->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料编辑成功！');
     }
 }
