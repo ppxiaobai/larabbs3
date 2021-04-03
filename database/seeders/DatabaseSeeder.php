6e8c4cb0ec5dd03190388af66d9f6efb619afa37
ppxiaobai <309997474@qq.com>
2021-04-03 08:37:30 +0000

    回复数据

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
+        $this->call(RepliesTableSeeder::class);
     }
 }
