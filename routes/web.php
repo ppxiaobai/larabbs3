8b56fcb1ad4013875f906c3172adffd8d1f4d10c
ppxiaobai <309997474@qq.com>
2021-04-03 04:28:23 +0000

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,6 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 话题分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
