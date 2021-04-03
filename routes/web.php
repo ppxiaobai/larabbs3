4e303ef48940599df748a5db584c7d8b2370e1d5
ppxiaobai <309997474@qq.com>
2021-04-03 07:57:56 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -39,10 +39,13 @@ Route::post('email/resend', 'Auth\VerificationController@resend')->name('verific
 // 个人页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 // 话题分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 // 上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+// seo 友好的url
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
